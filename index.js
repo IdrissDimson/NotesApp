b0f61cd6fbc4519724655f6c35846025d024ca38
Idriss Dimson <idriss.dimson@gmail.com>
2021-05-24 21:28:57 -0400

    added comments to the code

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-// let submitButton = document.getElementById("notes-submit");
-// let notesContent = document.getElementById("notes-content");
-
-// let newContent = document.getElementById("new-notes-content");
-// let editButton = document.getElementById("notes-edit");
-// let addContent = document.getElementById("edit-notes-div");
-// let notesDiv = document.getElementById("create-notes-div");
-
-// let deleteNote = document.getElementById("delete-note");
-
-// editButton.addEventListener("click", function() {
-//   addContent.style.display = "block";
-// })
-// submitButton.addEventListener("click", function() {
-//   addNewText(newContent, notesContent);
-// });
-// deleteNote.addEventListener("click", function() {
-//   notesDiv.parentNode.removeChild(notesDiv);
-// })
 let counter = 0;
 let oldDiv;
 let selected = false;
+
 let addNotes = document.getElementById("add-notes");
 let notesGrid = document.getElementById("grid-notes");
 let notesArray = document.getElementsByClassName("notes");
 
 addNotes.addEventListener("click", function() {
-  if(counter < 7774){
+  if(counter < 7774){ // if the counter doesn't exceeds the limit of 7774 notes ...
     counter++;
     createNewNotes(counter);
     buttonListeners(counter);
@@ -35,6 +17,7 @@ addNotes.addEventListener("click", function() {
 })
 
 function createNewNotes(index) {
+  // Creation of the notes div
   let notesDiv = document.createElement("div");
   let contentDiv = document.createElement("div");
 
@@ -47,6 +30,7 @@ function createNewNotes(index) {
   let deleteButton = document.createElement("button");
   let submitButton = document.createElement("button");
 
+  // setting the classes, id's, and text if applicable
   notesDiv.setAttribute("class", "notes");
   notesDiv.setAttribute("id", `add-notes-${index.toString()}`);
 
@@ -69,6 +53,7 @@ function createNewNotes(index) {
   contentTextArea.setAttribute("maxlength", "255");
   contentTextArea.setAttribute("id", `new-notes-content-${index.toString()}`);
   
+  // adding the elements to the notes div and then the page itself
   notesDiv.appendChild(para);
   notesDiv.appendChild(editButton);
   notesDiv.appendChild(deleteButton);
@@ -82,6 +67,7 @@ function createNewNotes(index) {
 }
 
 function buttonListeners(index) {
+  // listens for clicks on the buttons
   let submitButton = document.getElementById(`notes-submit-${index.toString()}`);
   let notesContent = document.getElementById(`notes-content-${index.toString()}`);
 
@@ -91,6 +77,10 @@ function buttonListeners(index) {
   let notesDiv = document.getElementById(`add-notes-${index.toString()}`);
 
   let deleteNote = document.getElementById(`delete-note-${index.toString()}`);
+
+  // if the div is selected, highlight the note. If another div was selected
+  // previously, deselect the divs and have the new note replace its note content
+  // with the old notes content. if the user selected the same div, deselect the note.
   notesDiv.addEventListener("click", function() {
     if(selected){
       notesDiv.classList.remove('selected');
@@ -110,18 +100,20 @@ function buttonListeners(index) {
       oldDiv = notesDiv;
     }
   })
-  
+  // display the edit note section of the note
   editButton.addEventListener("click", function() {
     addContent.style.display = "block";
   })
+  // add the new text into the notes
   submitButton.addEventListener("click", function() {
     addNewText(newContent, notesContent, addContent);
   });
+  // delete the note from the page
   deleteNote.addEventListener("click", function() {
     notesDiv.parentNode.removeChild(notesDiv);
   })
 }
-
+// add the text from the textarea to the note's content p, as well as hides the textarea.
 function addNewText(contentDiv, targetDiv, currentDiv) {
   targetDiv.innerText =  contentDiv.value;
   contentDiv.value = "";
